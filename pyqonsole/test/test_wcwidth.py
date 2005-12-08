c23096a4f50a4dbd04822ae59f209407b6c364a8
alf <alf@localhost>
2005-12-08 16:13:39 +0000

    [project @ 2005-12-08 17:13:39 by alf] added tests and found why some of the tests fail. We can probably live with the failures, though.

--- pyqonsole/test/test_wcwidth.py
+++ pyqonsole/test/test_wcwidth.py
@@ -1,30 +1,53 @@
 #!/usr/bin/env python2.4
 
-__revision__ = '$Id: test_wcwidth.py,v 1.3 2005-12-08 14:28:45 alf Exp $'
+__revision__ = '$Id: test_wcwidth.py,v 1.4 2005-12-08 17:13:39 alf Exp $'
 
-## XXX FIXME: find out if the failing tests are caused by:
-##  * Errors in the test
-##  * Errors in the helpers.c module
-##  * Errors in the unicodedata module from stdlib
 
-## TODO : use http://www.dpawson.co.uk/xsl/rev2/UnicodeCategories.html
-## for a better use of the category names
-
-## Also give a look at http://www.unicode.org/faq/specifications.html
-## for more info in finding who's wrong and who's right in the tests
-
+## http://www.cl.cam.ac.uk/~mgk25/ucs/scw-proposal.html is the right place
 
 from pyqonsole.helpers import wcWidth
 import unittest
-import unicodedata
+import unicodedata as udata
 
 def makeCats():
     cats = {}
     for i in xrange(0x110000):
         char = unichr(i)
-        cats.setdefault(unicodedata.category(char), []).append(i)
+        cats.setdefault(udata.category(char), []).append(i)
     return cats
 
+category_names = {'Cc': 'Other, control',
+                  'Cf': 'Other, format',
+                  'Cn': 'Other, not assigned',
+                  'Co': 'Other, private use',
+                  'Cs': 'Other, surrogate',
+                  'Ll': 'Letter, lowercase',
+                  'Lm': 'Letter, modifier',
+                  'Lo': 'Letter, other',
+                  'Lt': 'Letter, titlecase',
+                  'Lu': 'Letter, uppercase',
+                  'Mc': 'Mark, spacing combining',
+                  'Me': 'Mark, enclosing',
+                  'Mn': 'Mark, non-spacing',
+                  'Nd': 'Number, decimal digit',
+                  'Nl': 'Number, letter',
+                  'No': 'Number other',
+                  'Pc': 'Punctuation, connector',
+                  'Pd': 'Punctuation, dash',
+                  'Pe': 'Punctuation, close',
+                  'Pf': 'Punctuation, final quote',
+                  'Pi': 'Puntuation, initial quote',
+                  'Po': 'Punctuation, other',
+                  'Ps': 'Punctuation, open',
+                  'Sc': 'Symbol, currency',
+                  'Sk': 'Symbol, modifier',
+                  'Sm': 'Symbol, math',
+                  'So': 'Symbol, other',
+                  'Zl': 'Seaprator, line',
+                  'Zp': 'Seaparator, paragraph',
+                  'Zs': 'Separator, space',
+                  }
+    
 CATS = makeCats()
 
 def widths():
@@ -34,8 +57,33 @@ def widths():
         print cat
         print '-'*80
         for c in chars:
-            print '%06X'%c, wcWidth(c), unicodedata.east_asian_width(unichr(c))
+            print '%06X'%c, wcWidth(c), udata.east_asian_width(unichr(c))
         print
+
+single_width = []
+double_width = []
+for cp in xrange(33,0x110000):
+    ch = unichr(cp)
+    if (udata.category(ch) in ('Mn', 'Me',  'Cf', 'Cn') and cp != 0xAD) or \
+           (0x7f<= cp < 0xa0) or \
+           (0x1160 <= cp <= 0x11ff) or \
+           (cp == 0x200B):
+        continue
+    if cp >= 0x1100 and (
+        cp <= 0x115f or \
+        cp in (0x2329, 0x232A) or \
+        (0x2e80 <= cp < 0x303f) or \
+        (0x303f < cp <= 0xa4cf) or \
+        (0xac00 <= cp <= 0xd7a3) or \
+        (0xf900 <= cp <= 0xfaff) or \
+        (0xfe30 <= cp <= 0xfe6f) or \
+        (0xff00 <= cp <= 0xff60) or \
+        (0xffe0 <= cp <= 0xffe6) or \
+        (0x20000 <= cp <= 0x2fffd) or \
+        (0x30000 <= cp <= 0x3fffd)):
+        double_width.append(cp)
+    else:
+        single_width.append(cp)
 
 class Wcwidth_TC(unittest.TestCase):
     
@@ -43,22 +91,27 @@ class Wcwidth_TC(unittest.TestCase):
         self.assertEquals(wcWidth(0), 0)
 
     def check_range(self, codepoints, expected):
+        failures = []
         for cp in codepoints:
             unichar = unichr(cp)
-            self.assertEquals(wcWidth(cp), expected,
-                              'codepoint %X [%s] [%s] width %d expected %d' % \
+##             print cp, udata.name(unichar, 'XXX Unknown Code Point')
+            if wcWidth(cp) != expected:
+                failures.append('codepoint %X [%s] [cat: %s (%s)] width %d expected %d' % \
                               (cp,
-                               unicodedata.name(unichar, 'XXX Unknown Code Point'),
-                               unicodedata.category(unichar),
+                               udata.name(unichar, 'XXX Unknown Code Point'),
+                               udata.category(unichar),
+                               category_names[udata.category(unichar)],
                                wcWidth(cp),
                                expected))
+        self.assertEquals(failures, [])
             
 
     def testControlChar(self):
-        codepoints = [cp for cp in CATS['Cc'] if cp != 0]
+        codepoints = range(1,32) + range(0x7f, 0xa0)
         self.check_range(codepoints, -1)
             
     def testFormatChar(self):
+        # This test fails because of problems in unidata
         self.check_range(CATS['Cf'], 0)
 
     def testCombiningChar(self):
@@ -66,30 +119,43 @@ class Wcwidth_TC(unittest.TestCase):
         self.check_range(CATS['Me'], 0)
             
 
-    def testWideChars(self):
-        codepoints = [cp for cp in xrange(0x110000)
-                          if unicodedata.east_asian_width(unichr(cp)) in ('F', 'W')]
-        self.check_range(codepoints, 2)
-
-    def testHangulJame(self):
+    def testHangulJamo(self):
         codepoints = xrange(0x1160, 0x11FF+1)
         self.check_range(codepoints, 0)
 
+    def testZeroWidthSpace(self):
+        self.check_range([0x200B], 0)
+
+    def testWideChars(self):
+        self.check_range(double_width, 2)
+
+
     def testRemainingChars(self):
-        # all available cats:
-        # ['Ps', 'Nl', 'No', 'Lo', 'Ll', 'Lm', 'Nd', 'Pc', 'Lt', 'Lu',
-        # 'Pf', 'Pd', 'Pe', 'Pi', 'Po', 'Me', 'Mc', 'Mn', 'Sk', 'So',
-        # 'Sm', 'Sc', 'Zl', 'Co', 'Cn', 'Cc', 'Cf', 'Cs', 'Zp', 'Zs']
-
-        for cat in ('Nd', 'Po', 'Lu', 'Pe', 'Ps', 'Ll', 'Lm'): # FIXME: add missing cats
-            codepoints = [cp for cp in CATS[cat]
-                          if unicodedata.east_asian_width(unichr(cp)) not in ('F', 'W')]
-            self.check_range(codepoints, 1)
+        # This test fails because of problems in unidata
+        self.check_range(single_width, 1)
                                                
+class UnidataErrors_TC(unittest.TestCase):
+    """The tests below fail because of an error in unicodedata.
 
-## for i in xrange(10):
-##     print wcWidth(i)
+    The category mappings are not comformant with
+    http://www.unicode.org/Public/UNIDATA/UnicodeData.txt maybe it is
+    a version mismatch (3.2 for unicodedata, 4.X for the file above)."""
+    
+    def test_MongolianVowelSeparator(self):
+        char = unichr(0x180E)
+        self.assertEquals(udata.name(char), 'MONGOLIAN VOWEL SEPARATOR')
+        self.assertEquals(udata.category(char), 'Zs')
+
+    def test_KhmerVowelInherentAa(self):
+        char = unichr(0x17B5)
+        self.assertEquals(udata.name(char), 'KHMER VOWEL INHERENT AA')
+        self.assertEquals(udata.category(char), 'Cf')
+
+    def test_KhmerVowelInherentAq(self):
+        char = unichr(0x17B4)
+        self.assertEquals(udata.name(char), 'KHMER VOWEL INHERENT AQ')
+        self.assertEquals(udata.category(char), 'Cf')
+
 if __name__ == '__main__':
-#    widths()
     unittest.main()
 
