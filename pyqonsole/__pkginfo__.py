c4c7ed083d476808fc9db762de93117db28f3693
alf <alf@localhost>
2005-12-20 13:30:51 +0000

    [project @ 2005-12-20 14:30:51 by alf] use logilab templates for distribution

---
+++ pyqonsole/__pkginfo__.py
@@ -0,0 +1,35 @@
+# pylint: disable-msg=W0622
+"""pyqonsole packaging information"""
+
+__revision__ = '$Id: __pkginfo__.py,v 1.1 2005-12-20 14:30:51 alf Exp $'
+
+modname="pyqonsole"
+numversion = (1, 0, 0)
+version = '.'.join([str(num) for num in numversion])
+license = 'CeCILL'
+copyright = '''WRITEME'''
+
+short_desc = "console application written in Python using Qt"
+long_desc = """\
+ WRITEME"""
+
+author = "Sylvain Thenault"
+author_email = "sylvain.thenault@logilab.fr"
+web = "http://www.logilab.org/projects/%s" % modname
+ftp = "ftp://ftp.logilab.org/pub/%s" % modname
+mailinglist = "mailto://python-projects@logilab.org"
+
+from os.path import join
+scripts = [join('bin', 'pyqonsole')]
+
+try:
+    from distutils.core import Extension
+    ext_modules=[Extension('pyqonsole._helpers',
+                     sources = ['helpers.c'])]
+except:
+    pass
+
+pyversions = ['2.3', '2.4']
+
+debian_maintainer = 'Alexandre Fayolle'
+debian_maintainer_email = 'afayolle@debian.org'
--- pyqonsole/setup.py
+++ pyqonsole/setup.py
@@ -1,12 +1,149 @@
-__revision__ = '$Id: setup.py,v 1.2 2005-12-09 09:11:13 alf Exp $'
+__revision__ = '$Id: setup.py,v 1.3 2005-12-20 14:30:51 alf Exp $'
 
-from distutils.core import setup, Extension
+import os
+import sys
+import shutil
+from distutils.core import setup
+from distutils.command import install_lib
+from os.path import isdir, exists, join, walk
 
-_helpers = Extension('_helpers',
-                     sources = ['helpers.c'])
+from __pkginfo__ import modname, version, license, short_desc, long_desc, \
+     web, author, author_email
+# import optional features
+try:
+    from __pkginfo__ import distname
+except ImportError:
+    distname = modname
+try:
+    from __pkginfo__ import scripts
+except ImportError:
+    scripts = []
+try:
+    from __pkginfo__ import data_files
+except ImportError:
+    data_files = None
+try:
+    from __pkginfo__ import subpackage_of
+except ImportError:
+    subpackage_of = None
+try:
+    from __pkginfo__ import include_dirs
+except ImportError:
+    include_dirs = []
+try:
+    from __pkginfo__ import ext_modules
+except ImportError:
+    ext_modules = None
 
-setup (name = 'pyqonsole',
-       version = '1.0',
-       description = '',
-       ext_modules = [_helpers])
+BASE_BLACKLIST = ('CVS', 'debian', 'dist', 'build', '__buildlog')
+IGNORED_EXTENSIONS = ('.pyc', '.pyo', '.elc')
 
+def get_packages(directory, prefix):
+    """return a list of subpackages for the given directory
+    """
+    result = []
+    for package in os.listdir(directory):
+        absfile = join(directory, package)
+        if isdir(absfile):
+            if exists(join(absfile, '__init__.py')) or \
+                   package in ('test', 'tests'):
+                if prefix:
+                    result.append('%s.%s' % (prefix, package))
+                else:
+                    result.append(package)
+                result += get_packages(absfile, result[-1])
+    return result
+
+
+def export(from_dir, to_dir,
+           blacklist=BASE_BLACKLIST,
+           ignore_ext=IGNORED_EXTENSIONS):
+    """make a mirror of from_dir in to_dir, omitting directories and files
+    listed in the black list
+    """
+    def make_mirror(arg, directory, fnames):
+        """walk handler"""
+        for norecurs in blacklist:
+            try:
+                fnames.remove(norecurs)
+            except ValueError:
+                pass
+        for filename in fnames:
+            # don't include binary files
+            if filename[-4:] in ignore_ext:
+                continue
+            if filename[-1] == '~':
+                continue
+            src = '%s/%s' % (directory, filename)
+            dest = to_dir + src[len(from_dir):]
+            print >> sys.stderr, src, '->', dest
+            if os.path.isdir(src):
+                if not exists(dest):
+                    os.mkdir(dest)
+            else:
+                if exists(dest):
+                    os.remove(dest)
+                shutil.copy2(src, dest)
+    try:
+        os.mkdir(to_dir)
+    except OSError, ex:
+        # file exists ?
+        import errno
+        if ex.errno != errno.EEXIST:
+            raise
+    walk(from_dir, make_mirror, None)
+
+
+EMPTY_FILE = '"""generated file, don\'t modify or your data will be lost"""\n'
+class MyInstallLib(install_lib.install_lib):
+    """extend install_lib command to handle  package __init__.py and
+    include_dirs variable if necessary
+    """
+    def run(self):
+        """overriden from install_lib class"""
+        install_lib.install_lib.run(self)
+        # create Products.__init__.py if needed
+        if subpackage_of:
+            product_init = join(self.install_dir, subpackage_of, '__init__.py')
+            if not exists(product_init):
+                self.announce('creating %s' % product_init)
+                stream = open(product_init, 'w')
+                stream.write(EMPTY_FILE)
+                stream.close()
+        # manually install included directories if any
+        if include_dirs:
+            if subpackage_of:
+                base = join(subpackage_of, modname)
+            else:
+                base = modname
+            for directory in include_dirs:
+                dest = join(self.install_dir, base, directory)
+                export(directory, dest)
+        
+def install(**kwargs):
+    """setup entry point"""
+    if subpackage_of:
+        package = subpackage_of + '.' + modname
+        kwargs['package_dir'] = {package : '.'}
+        packages = [package] + get_packages(os.getcwd(), package)
+    else:
+        kwargs['package_dir'] = {modname : '.'}
+        packages = [modname] + get_packages(os.getcwd(), modname)
+    kwargs['packages'] = packages
+    return setup(name = distname,
+                 version = version,
+                 license =license,
+                 description = short_desc,
+                 long_description = long_desc,
+                 author = author,
+                 author_email = author_email,
+                 url = web,
+                 scripts = scripts,
+                 data_files=data_files,
+                 ext_modules=ext_modules,
+                 cmdclass={'install_lib': MyInstallLib},
+                 **kwargs
+                 )
+            
+if __name__ == '__main__' :
+    install()
