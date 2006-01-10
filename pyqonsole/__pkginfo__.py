d3833a3d611afab4a9e1832544bf556ba3aae948
syt <syt@localhost>
2006-01-10 08:51:16 +0000

    [project @ 2006-01-10 09:51:15 by syt] packaging

--- pyqonsole/__pkginfo__.py
+++ pyqonsole/__pkginfo__.py
@@ -1,13 +1,15 @@
 # pylint: disable-msg=W0622
 """pyqonsole packaging information"""
 
-__revision__ = '$Id: __pkginfo__.py,v 1.6 2006-01-05 13:38:59 alf Exp $'
+__revision__ = '$Id: __pkginfo__.py,v 1.7 2006-01-10 09:51:16 syt Exp $'
 
 modname = "pyqonsole"
 numversion = (0, 1, 0)
 version = '.'.join([str(num) for num in numversion])
 license = 'CeCILL'
-copyright = '''WRITEME'''
+copyright = '''Copyright (c) 2006 LOGILAB S.A. (Paris, FRANCE)
+Copyright (c) 2006 CEA-Grenoble.
+'''
 
 short_desc = "console application written in Python using Qt"
 long_desc = """\
